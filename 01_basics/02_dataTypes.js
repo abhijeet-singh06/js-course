e89f47ecfbd1196891009115b7ef8a7353d25f45
Abhijeet Singh <viraj555singh@gmail.com>
2026-01-07 12:30:57 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+3) // gives error cause we are using nodejs, not browser
+
+console.log(3+3);
+console.log("Viraj");
+
+let name = "Viraj";
+let age = 22;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint => larger than 2 to power 53
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined => variable without value
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof null); // bug in js, it shows object
+console.log(typeof state);
