029bb54ae036ae41e218f244a1ca59b12e327fb2
Abhijeet Singh <viraj555singh@gmail.com>
2026-01-08 10:47:24 +0000

    imp comparisons

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -61,4 +61,10 @@ console.log(null == 0);
 console.log(null >= 0);
 console.log(null == undefined);
 
-// 
+// thes reason is that an equality check(==) and comparisons >, <, >=, <= work differently.
+// For comparisons, JavaScript converts null to a number, treating it as 0. thats why null >= 0 is true.
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+// in case of undefined, it is not converted to any number, so all comparisons except != and !== result in false.
