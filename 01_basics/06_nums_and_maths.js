3389d79f18519ae9fb780f5aa4c19e076477e0a8
Abhijeet Singh <viraj555singh@gmail.com>
2026-01-10 18:01:38 +0000

    maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -18,3 +18,22 @@ console.log(hundreds.toLocaleString('en-IN'));
 
 
 //********************** Maths ************************ */
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(2, 33, 4, 1, 55, -23));
+console.log(Math.max(2, 33, 4, 1, 55, -23));
+
+
+console.log(Math.random()); 
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10) + 1));
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
