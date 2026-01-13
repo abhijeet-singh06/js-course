2d97992e6e439ebd4b24f35f85696426baffe75d
Abhijeet Singh <viraj555singh@gmail.com>
2026-01-13 15:05:54 +0000

    obj continued

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -26,15 +26,42 @@ const obj1 = {1: "a", 2: "b"};
 const obj2 = {3: "c", 4: "d"};
 
 // const obj3 = {obj1, obj2};
-const obj3 = Object.assign(obj1, obj2);  // mutates obj1
+const obj3 = Object.assign(obj1, obj2);  // obj1 will act as target object and will be mutated
+
+const obj5 = Object.assign({}, obj1, obj2);  // to avoid mutation of obj1 and target will be empty object
+
 const obj4 = {...obj1, ...obj2};   // spread operator
-const obj5 = Object.assign({}, obj1, obj2);  // to avoid mutation of obj1
 
 console.log(obj3);
 console.log(obj4);
 console.log(obj5);
 
+ 
 
+const users = [
+    {
+        userId: 1,
+        userName: "Viraj",
+    },
+    {
+        userId: 2,
+        userName: "Viraj",
+    },
+    {
+        userId: 3,
+        userName: "Viraj",
+    },
+]
+
+console.log(users[1].userId);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));   // tells whether the object has the specified property as its own property
 
 
 
