3ef78b54e532a6587c5511107bfa2f6f79d7358f
Abhijeet Singh <viraj555singh@gmail.com>
2026-01-16 05:46:46 +0000

    scopes level

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,11 +4,55 @@ let a = 300;
 if(true) {
     let a = 10;
     const b = 20;
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
 
+
+function one() {
+    const username = "viraj";
+
+    function two() {
+        const website = "yt";
+        console.log(username);
+    }
+    // console.log(website);
+
+    two();
+}
+
+// one();
+
+
+if (true) {
+    const username = "viraj";
+    if (username === "viraj") {
+        const website = " chrome";
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+
+// ++++++++++++++++ interesting +++++++++++++++++++
+
+console.log(addone(5));    // can access it if it's being declared like below 
+function addone(num){
+    return num+1;
+}
+
+
+addTwo(5);          // can't access it before initialization
+const addTwo = function(num){
+    return num + 2;
+}
+
+
+  
