67892d21ccd963bb722d5c2ba0a96d2e8760d0a0
Abhijeet Singh <viraj555singh@gmail.com>
2026-01-17 13:55:50 +0000

    theory of JS execution context

---
+++ 03_basics/05_JS_execution_context.js
@@ -0,0 +1,9 @@
+// Javascript Execution Context
+
+// An execution context is an abstract concept that holds information about the environment within which the current code is being executed.
+
+
+// There are three types of execution contexts in JavaScript:
+// 1. Global Execution Context
+// 2. Functional Execution Context
+// 3. Eval Execution Context
