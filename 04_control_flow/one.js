adf55dfaddd1cdc64011c54dca9dc2ca0f1b252a
Abhijeet Singh <viraj555singh@gmail.com>
2026-01-18 17:43:45 +0000

    control flow basic

---
+++ 04_control_flow/one.js
@@ -0,0 +1,56 @@
+// if 
+const isUserLoggedIn = true;
+const temperature = 41;
+
+//  if(temperature === 40) {      // checks data type as well
+//     console.log("temperature is less than 50");
+//  } else{
+//     console.log("temperature is greater than 50");
+//  }
+// console.log("It's a moderate day");
+
+
+
+// const score = 200;
+// if(score > 100) {
+//     const power = "fly";
+//     console.log(`User power: ${power}`);
+// }
+ 
+// console.log(`User power: ${power}`); // error: power is not defined
+
+
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
