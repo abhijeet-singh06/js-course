aff897f2dfde0c33e1c7a8004350602e602a9130
Abhijeet Singh <viraj555singh@gmail.com>
2026-01-20 18:51:04 +0000

    maps and obj

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -12,8 +12,38 @@
 
 
 
-const greetings = "Hello World!";
+// const greetings = "Hello World!";
 
-for (const greet of greetings) {
-  console.log(`Each letter: ${greet}`);
+// for (const greet of greetings) {
+//   console.log(`Each letter: ${greet}`);
+// }
+
+
+
+// Maps
+// const map = new Map();
+// map.set('IN', "India");
+// map.set('US', "United States");
+// map.set('FR', "France");
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//   console.log(key, ':-', value);
+// }
+
+
+
+
+
+// Objects
+const myObj = {
+  'game1': 'Chess',
+  'game2': 'Football',
+  'game3': 'Tennis'
 }
+
+for (const [key, value] of myObj) {
+  console.log(key, ':-', value);
+}
+// TypeError: myObj is not iterable
